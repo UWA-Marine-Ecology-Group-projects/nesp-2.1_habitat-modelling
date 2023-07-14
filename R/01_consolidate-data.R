71eba3d3c0a0dc6cc159f816e943148d6b29f5fa
claudehspencer <claudespencer111@gmail.com>
2023-07-14 08:02:08 +0800

    fixed metadata and tidy output

--- R/01_consolidate-data.R
+++ R/01_consolidate-data.R
@@ -31,7 +31,7 @@ habitat <- list.files(path = "data/staging",
                    broad.total.points.annotated, broad.unknown)) %>%
   dplyr::filter(campaignid %in% c("2020-06_south-west_stereo-BRUVs", 
                                   "2020-10_south-west_stereo-BRUVs",
-                                  "2020-10_south-west_BOSS",
+                                  # "2020-10_south-west_BOSS",
                                   "2021-03_West-Coast_BOSS" )) %>%
   dplyr::filter(!sample %in% "NA") %>%                                          # One NA sample in the FRDC campaign
   mutate(across(starts_with("broad"), as.numeric)) %>%                          # Change back to numeric values
@@ -47,20 +47,53 @@ habitat <- list.files(path = "data/staging",
 # Read in the metadata
 metadata <- list.files(path = "data/raw",
                        recursive = T,
-                       pattern = "_checked.metadata.csv",
+                       pattern = "_Metadata.csv",
                        full.names = T) %>%
   purrr::map_dfr(~read_files_csv(.)) %>%
   dplyr::filter(campaignid %in% c("2020-06_south-west_stereo-BRUVs", 
                                   "2020-10_south-west_stereo-BRUVs",
-                                  "2020-10_south-west_BOSS",
+                                  # "2020-10_south-west_BOSS",
                                   "2021-03_West-Coast_BOSS" )) %>%
-  dplyr::select(campaignid, sample, planned.or.exploratory) %>%
-  dplyr::mutate(planned.or.exploratory = ifelse(campaignid %in% "2021-03_West-Coast_BOSS", 
-                                                "MBH", planned.or.exploratory)) %>%
+  dplyr::select(campaignid, sample, site, planned.or.exploratory) %>%
+  dplyr::filter(!is.na(sample)) %>%
+  dplyr::mutate(sample = ifelse(campaignid %in% "2020-06_south-west_stereo-BRUVs" 
+                                & str_length(.$sample) == 1, 
+                                str_pad(.$sample, width = 2, 
+                                        pad = "0", side = "left"), 
+                                sample)) %>%
+  distinct() %>%                                                                # Duplicated row
   glimpse()
 
-unique(metadata$campaignid)
-  
+test <- metadata %>%
+  dplyr::filter(is.na(planned.or.exploratory))
+
+# missing.sites <- metadata %>%
+#   dplyr::filter(campaignid %in% "2020-10_south-west_stereo-BRUVs") %>%
+#   dplyr::mutate(sample = str_replace_all(.$sample, "IO", "")) %>%
+#   dplyr::select(-campaignid) %>%
+#   glimpse()
+# 
+# missing.mbh <- metadata %>%
+#   dplyr::filter(is.na(planned.or.exploratory)) %>%
+#   dplyr::select(-c(planned.or.exploratory, site)) %>%
+#   dplyr::left_join(missing.sites) %>%
+#   glimpse()
+# 
+# test <- missing.mbh %>%
+#   dplyr::filter(is.na(planned.or.exploratory))
+
+# Join habitat data with cluster ID (site) and mark type (MBH or other)
+
+tidy.habitat <- habitat %>%
+  left_join(metadata) %>%
+  dplyr::mutate(planned.or.exploratory = ifelse(is.na(planned.or.exploratory),
+                                                "Captains pick", planned.or.exploratory)) %>%
+  dplyr::mutate(clustered = ifelse(campaignid %in% "2021-03_West-Coast_BOSS", FALSE, TRUE)) %>%
+  glimpse()                                                                     # Number of obs matches original
+
+test <- tidy.habitat %>%
+  dplyr::filter(is.na(planned.or.exploratory) & isTRUE(clustered))
+
 # Write out the tidy data
 write.csv(habitat, file = "data/tidy/nesp-2.1_WA_habitat-data.csv",
           row.names = F)
